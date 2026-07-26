e7721062f8a7192255ecfa6c98ed9448806cee84
cagataykalkan <1Dahanah>
2026-07-27 00:29:20 +0300

    Initial Commit

---
+++ Versus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Versus
+//
+//  Created by Çağatay KALKAN on 27.07.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
